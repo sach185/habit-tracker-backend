e78bc9d7f6a79e312f128cbfe345a52a442b9249
Sachin <sachinn185@gmail.com>
2022-04-25 19:58:52 +0530

    env for mongodb

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
   getDbConnectionString: function () {
-    return "mongodb+srv://sachin:mongo%40test123@cluster0.ssxpp.mongodb.net/HabitDB?retryWrites=true&w=majority";
+    return (
+      process.env.MONGO_URI ||
+      "mongodb+srv://sachin:mongo%40test123@cluster0.ssxpp.mongodb.net/HabitDB?retryWrites=true&w=majority"
+    );
   },
 };
